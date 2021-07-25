ccd96266cf3136a7daa3903a7a35c71abec74d75
GovardhanR26 <govardhangajubali@gmail.com>
2021-07-25 19:37:08 +0530

    removed unwanted submodule

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,19 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 const config = {
-  apiKey: "Your API Key here",
-  authDomain: "blah",
-  projectId: "blah",
-  storageBucket: "blah",
-  messagingSenderId: "blah",
-  appId: "blah",
+  // apiKey: "Your API Key here",
+  // authDomain: "blah",
+  // projectId: "blah",
+  // storageBucket: "blah",
+  // messagingSenderId: "blah",
+  // appId: "blah",
+
+  apiKey: "AIzaSyDgRJ4sHJ6MZY3vRAoSUPojP98NIjvRymQ",
+  authDomain: "chateasy-7569b.firebaseapp.com",
+  projectId: "chateasy-7569b",
+  storageBucket: "chateasy-7569b.appspot.com",
+  messagingSenderId: "601261175079",
+  appId: "1:601261175079:web:20b4071fd7b55cb9dab902",
 };
 
 firebase.initializeApp(config);
